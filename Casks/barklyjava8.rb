69827fa6ac76a36379c7fc5fe6da9de4dd35d243
Mahesh Immanni <mahesh.immanni@alertlogic.com>
2020-12-07 14:39:04 +0530

    move away from un-available java version

--- Casks/barklyjava8.rb
+++ Casks/barklyjava8.rb
@@ -1,6 +1,6 @@
 cask 'barklyjava8' do
   version '1.8.0_271-b09,61ae65e088624f5aaa0b1d2d801acb16'
-  sha256 'b0de04d3ec7fbf2e54e33e29c78ababa0a4df398ba490d4abb125b31ea8d663e'
+  sha256 '6ba0a30eeddaad7259fefce09a76a5f5f8dabf5d745c4110f921d2a5fbf7307d'
 
   java_update = version.sub(%r{.*_(\d+)-.*}, '\1')
   url "http://download.oracle.com/otn-pub/java/jdk/#{version.minor}u#{version.before_comma.split('_').last}/#{version.after_comma}/jdk-#{version.minor}u#{java_update}-macosx-x64.dmg",
